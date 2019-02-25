40215be8e5f43f07edad8b115c42aa47d9bbe0c5
Edward Njoroge <edwardnjorogekahara@gmail.com>
2019-02-25 18:28:50 +0300

    remove unnecessary imports from App.js

--- App.js
+++ App.js
@@ -1,10 +1,4 @@
 import React, {Component} from 'react';
-import {
-  Button,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
 import {createStackNavigator, createAppContainer} from "react-navigation";
 import {Home} from './screens/home';
 import {Settings} from './screens/settings';
